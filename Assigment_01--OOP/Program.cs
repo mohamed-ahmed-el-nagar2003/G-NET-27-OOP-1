b59f0c9e45ba07fcd8146f1473d0406742c4e0b7
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-02-21 17:03:47 +0200

    Q-01

--- Assigment_01--OOP/Program.cs
+++ Assigment_01--OOP/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q-01
+            //Q1 : Explain with code example how class and struct behave differently 
+            //Student st01 = new Student("Mohamed ", 20, "Science", "20202020");
+            //Student st02 = new Student("Ali ", 40, "Science", "80808080");
+            //Student st03 = st01;
+            //Console.WriteLine($"{st01.Name}");
+            //Console.WriteLine($"{st03.Name}");
+            //st03.Name = "Sammer";
+            ////st01 will change also Although we change st03
+            ////becouse Refrence type 
+            //Console.WriteLine($"{st01.Name}");
+            //Console.WriteLine($"{st03.Name}");
+
+
+
+            //Dimention dim01 = new Dimention(3, 4);
+            //Dimention dim02 = dim01;
+            //Console.WriteLine($"{dim01.X}");
+            //Console.WriteLine($"{dim02.X}");
+            //dim02.X = 8;
+
+            ////dim01 will not change also Although we change dim02 becouse Refrence type
+            //Console.WriteLine($"{dim01.X}");
+            //Console.WriteLine($"{dim02.X}");
+            #endregion
         }
     }
 }
